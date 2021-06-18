9e54561906fe35319e0ddd29fbbcc78d3faa6dbf
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-06-18 11:34:18 +0100

    add cloudwatch config generator

---
+++ cloudwatch/remote-state.tf
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------
+### Getting the common details
+#-------------------------------------------------------------
+data "terraform_remote_state" "common" {
+  backend = "s3"
+
+  config = {
+    bucket = var.remote_state_bucket_name
+    key    = "cp-oracle/common/terraform.tfstate"
+    region = var.region
+  }
+}
+
+#-------------------------------------------------------------
+### Getting the EC2 details
+#-------------------------------------------------------------
+data "terraform_remote_state" "alb" {
+  backend = "s3"
+
+  config = {
+    bucket = var.remote_state_bucket_name
+    key    = "cp-oracle/ec2/terraform.tfstate"
+    region = var.region
+  }
+}
---
+++ cloudwatch/s3object.tf
@@ -0,0 +1,31 @@
+# Web App instance cloudwatch config file
+data "template_file" "APP_cloudwatch_config" {
+  template = file("./files/config.json")
+  vars = {
+    log_group_name                   = local.app_log_group_name
+    system_event_log_group_name      = local.app_system_events_log_group_name
+    application_event_log_group_name = local.app_application_events_log_group_name
+  }
+}
+
+resource "aws_s3_bucket_object" "APP_cloudwatch_config" {
+  bucket  = local.artifacts_s3_bucket_name
+  key     = "/cloudwatch/app_config.json"
+  content = data.template_file.APP_cloudwatch_config.rendered
+}
+
+# API instance cloudwatch config file
+data "template_file" "API_cloudwatch_config" {
+  template = file("./files/config.json")
+  vars = {
+    log_group_name                   = local.app_log_group_name
+    system_event_log_group_name      = local.app_system_events_log_group_name
+    application_event_log_group_name = local.app_application_events_log_group_name
+  }
+}
+
+resource "aws_s3_bucket_object" "API_cloudwatch_config" {
+  bucket  = local.artifacts_s3_bucket_name
+  key     = "/cloudwatch/api_config.json"
+  content = data.template_file.API_cloudwatch_config.rendered
+}
