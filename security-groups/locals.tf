7f2ea47879dd820e2eebd33ef08a2e362291ef46
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-06-18 11:57:07 +0100

    add dev bastion ip to allow list

--- security-groups/locals.tf
+++ security-groups/locals.tf
@@ -20,6 +20,5 @@ locals {
   ssh_deployer_key = data.terraform_remote_state.common.outputs.ssh_deployer_key
 
   #bastion_cidr = flatten(data.terraform_remote_state.common.outputs.bastion_cidr)
-  bastion_cidr = [ "10.161.98.0/28", "10.161.98.16/28", "10.161.98.32/28"]
-
+  bastion_cidr = [ "10.161.98.0/28", "10.161.98.16/28", "10.161.98.32/28", "${data.terraform_remote_state.bastion.outputs.bastion_ip}/32"]
 }
