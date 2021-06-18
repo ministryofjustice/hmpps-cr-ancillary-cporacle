bf8bf5cb77d39350600233f83c2d7dc1b49e67e8
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-06-18 14:19:52 +0100

    update db name on cw

--- alerts/locals.tf
+++ alerts/locals.tf
@@ -25,7 +25,7 @@ locals {
   }
 
   sns_alarm_notification_arn = data.terraform_remote_state.monitoring.outputs.aws_sns_topic_alarm_notification["arn"]
-  db_instance_id              = "cp-oracle-native-backup-restore"
+  db_instance_id              = "cp-oracle-native-backup-restore" //Hardcode as reference have some DB changes to apply, should be addressed later
   cporacle_api_asg            = data.terraform_remote_state.ec2.outputs.cporacle_app_asg["name"]
   cporacle_app_asg            = data.terraform_remote_state.ec2.outputs.cporacle_app_asg["name"]
   cporacle_api_alb            = data.terraform_remote_state.alb.outputs.alb_api
