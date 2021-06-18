1a82b83d402d20e96b745fb769dafafce3d146d6
BensonArokiadoss <benson.arokiadoss@adrocgroup.com>
2021-06-18 14:47:23 +0100

    update cw dashboard

--- alerts/dashboard.tf
+++ alerts/dashboard.tf
@@ -1,7 +1,16 @@
 data "template_file" "dashboard" {
   template = file("./templates/dashboard.json")
   vars = {
-    region = var.region
+    region                       = var.region
+    db_name                      = local.db_instance_id
+    app_asg_autoscale_name       = local.cporacle_app_asg
+    app_lb_arn_suffix            = local.cporacle_app_alb["arn_suffix"]
+    app_target_group_arn_suffix  = local.cporacle_app_tg["arn_suffix"]
+    app_app_pool_httperr_offline = aws_cloudwatch_metric_alarm.APP_iis_httperr.arn
+    api_asg_autoscale_name       = local.cporacle_api_asg
+    api_lb_arn_suffix            = local.cporacle_api_alb["arn_suffix"]
+    api_target_group_arn_suffix  = local.cporacle_api_tg["arn_suffix"]
+    api_app_pool_httperr_offline = aws_cloudwatch_metric_alarm.API_iis_httperr.arn
   }
 }
 
